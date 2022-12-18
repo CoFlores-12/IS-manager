f1c44b30801aca8e6497edcf56855e466eb79654
CoFlores-12 <obethflores2014@gmail.com>
2022-12-17 19:04:35 -0600

    test 01

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 const express = require('express');
 const app = express();
 require('dotenv').config()
+const chrome = require('chrome-aws-lambda');
 
 app.get('/', (req, res) => {
     res.send("init IS-manager")
 });
 
-app.listen(process.env.NODE_PORT, () => {
+var browser = null;
+var page = null;
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function miMiddleware(req,res,next){
+    //go to login page
+    page = await browser.newPage();
+    await page.goto('https://registro.unah.edu.hn/pregra_estu_login.aspx');
+    await sleep(4 * 1000);
+    next();
+}
+
+app.get('/login/:clave/:valor', miMiddleware, async function(req,res){
+   /* Se ejecutará esta función luego del middleware */
+   await sleep(6 * 1000);
+   res.send("init IS-manager")
+});
+
+app.listen(process.env.NODE_PORT, async () => {
+    browser = await chrome.puppeteer.launch({
+        args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+        defaultViewport: chrome.defaultViewport,
+        executablePath: await chrome.executablePath,
+        headless: true,
+        ignoreHTTPSErrors: true,
+    });
     console.log(`Server started on port ${process.env.NODE_PORT}`);
+    
 });
