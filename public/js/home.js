8f8a8c12f4b0424ee9251a0f99f0c4b81b429a1b
CoFlores-12 <obethflores2014@gmail.com>
2023-01-03 23:04:00 -0600

    frontend update

--- public/js/home.js
+++ public/js/home.js
@@ -1,6 +1,16 @@
 career=[];
 available=[];
 
+//install SW
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err));
+  });
+}
+
 (async ()=>{
   $('.name').text(data.INFO.Nombre)
   $('.career').text(data.INFO.Carrera)
