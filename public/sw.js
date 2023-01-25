8610b722f6efdc8b89da21a03d306012a6578688
CoFlores-12 <obethflores2014@gmail.com>
2023-01-24 22:10:51 -0600

    SW 1.4

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,11 @@
 const assets = [
     "/",
     "https://code.jquery.com/jquery-3.6.3.min.js",
+    "https://kit.fontawesome.com/1e8824e8c2.js",
     "https://is-manager.vercel.app/index.html",
     "https://is-manager.vercel.app/js/home.js",
-    "https://is-manager.vercel.app/css/css/index.css",
-    "https://is-manager.vercel.app/css/css/style.css",
+    "https://is-manager.vercel.app/css/index.css",
+    "https://is-manager.vercel.app/css/style.css",
     "https://is-manager.vercel.app/css/animate.min.css",
     "https://is-manager.vercel.app/logo.png",
     "https://is-manager.vercel.app/Spinner-1s-200px.svg"
