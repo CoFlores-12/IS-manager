e061363f0479be39162128ce577cb44bfc4ca5dd
CoFlores-12 <obethflores2014@gmail.com>
2023-08-21 18:25:36 -0600

    py

--- api/index.py
+++ api/index.py
@@ -9,3 +9,14 @@ async def main():
     await browser.close()
 
 asyncio.get_event_loop().run_until_complete(main())
+
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write('Hello, world!'.encode('utf-8'))
+        return
