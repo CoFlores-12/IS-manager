d830a8082ec549d00753a6b430fc144033eae9e2
Cesar Flores <48534199+CoFlores-12@users.noreply.github.com>
2023-08-22 22:52:31 -0600

    Update index.js

--- index.js
+++ index.js
@@ -22,150 +22,23 @@ let page = null;
 let pages = 0;
 const myBrowserlessAPIKey = process.env['TOKEN']
 
-
-async function openRegister(req,res,next){
-    console.time("open");
-    if (browser === null) {
-        browser = await puppeteer.connect({ browserWSEndpoint: 'wss://chrome.browserless.io?token='+myBrowserlessAPIKey});
-    }
-    //go to login page
-    page = await browser.newPage();
-    await page.goto('https://registro.unah.edu.hn/pregra_estu_login.aspx');
-    
-    console.timeEnd("open");
-    await next();
-}
-
-async function login(req,res,next){
-  console.time("login");
-    //login with credentials 
-     await page.type('#MainContent_txt_cuenta', req.body['cuenta']);
-     console.timeEnd("login");
-     await next();
-}
-
-async function login1(req,res,next){
-  console.time("login1");
-     await page.type('#MainContent_txt_clave', req.body['clave']);
-     console.timeEnd("login1");
-     await next();
-}
-
-async function login2(req,res,next){
-  console.time("login2");
-    await page.click('#MainContent_Button1');
-     console.timeEnd("login2");
-     await next();
-}
-
-async function login3(req,res,next){
-  console.time("login3");
-    try {
-      await page.waitForSelector('#MainContent_LinkButton2');
-
-    await page.click('#MainContent_LinkButton2');
-      console.timeEnd("login3");
-      await next();
-    } catch (error) {
-      res.status(500).send("Invalid Credentials");
-      return;
-    }
-}
-function sleep(ms) {
-
-return new Promise((resolve) => setInterval(resolve, ms));
-
-}
-async function prenumber(req,res,next){
-  console.time("prenumber");
-    await sleep(5000);
-  //await page.waitForSelector('#MainContent_ASPxPageControl1_ASPxGridView2_DXMainTable');
-     console.timeEnd("prenumber");
-     await next();
-}
-
-async function pageNumber(req,res,next){
-  console.time("number");
-  
-    
-  
-    //get number of pages in history
-    pages = await page.evaluate(() => {
-      const data = document.getElementsByClassName('dxpSummary_Aqua');
-      const myArray = data[0].innerHTML.split(" ");
-      return myArray[3];
-    });
-    console.timeEnd("number");
-    await next();
-}
-let classRes = {
-  "classes": [],
-  "INFO": {}
-};
-async function register1(req,res,next){
-  console.time("preregister");
-  classRes = {
-    "classes": [],
-    "INFO": {}
-  };
-  for (let i = 0; i < pages; i++) {
-    responseClass = await page.evaluate(() => { 
-      const  clases = [];
-      //get all elements of class table
-      const elements = document.querySelectorAll('#MainContent_ASPxPageControl1_ASPxGridView2_DXMainTable tbody tr');
-
-      for (let index = 9; index<elements.length; index++){
-        clases.push({
-            'CODIGO': elements[index].getElementsByTagName('td')[0].innerHTML,
-            'ASIGNATURA': elements[index].getElementsByTagName('td')[1].innerHTML,
-            'UV': elements[index].getElementsByTagName('td')[2].innerHTML,
-            'SECCION': elements[index].getElementsByTagName('td')[3].innerHTML,
-            'ANIO': elements[index].getElementsByTagName('td')[4].innerHTML,
-            'PERIODO': elements[index].getElementsByTagName('td')[5].innerHTML,
-            'CALIFICACION': elements[index].getElementsByTagName('td')[6].innerHTML,
-            'OBS': elements[index].getElementsByTagName('td')[7].innerHTML
-          });
-      }
-      return clases;
-    });
-    responseClass.forEach(clas => {
-      classRes.classes.push(clas);
-    });
-  //next page in history
-  await page.evaluate(() => {
-    aspxGVPagerOnClick("MainContent_ASPxPageControl1_ASPxGridView2","PBN");
-  });
-  await page.waitForTimeout(600);
-}
-    console.timeEnd("preregister");
-    await next();
-}
 app.post('/api/register', openRegister, login, login1,login2,login3,prenumber, pageNumber, register1, async function (req, res) {
 
-  console.time("register1");
-    //get averanges
-    const promedio = await page.evaluate(() => {
-      const obj = {
-          "Indice": {
-              'global': document.getElementById('MainContent_ASPxRoundPanel2_ASPxLabel11').innerHTML,
-              'periodo': document.getElementById('MainContent_ASPxRoundPanel2_ASPxLabel12').innerHTML
-          },
-          "Nombre": document.getElementById('MainContent_ASPxRoundPanel2_ASPxLabel8').innerHTML,
-          "Carrera": document.getElementById('MainContent_ASPxRoundPanel2_ASPxLabel9').innerHTML
-      };
-      return obj;
-    });
-    console.timeEnd("register1");
-    console.time("register2");
-    classRes.INFO=promedio;
-  
-    await page.close();
-    page = null;
-    req.session.number = req.body.cuenta;
-    req.session.key = req.body.clave;
-    console.timeEnd("register2");
-    res.send(classRes);
-    
+  await page.type('#MainContent_txt_cuenta', req.body['cuenta']);
+  await page.type('#MainContent_txt_clave', req.body['clave']);
+  await page.click('#MainContent_Button1');
+
+  //go to history
+  try {
+    await page.waitForSelector('#MainContent_LinkButton2');
+      req.session.number = req.body['cuenta'];
+      req.session.key = req.body['clave']
+  } catch (error) {
+    res.status(500).send("Invalid Credentials");
+    return;
+  }
+
+  res.send("ready");
 });
 
 app.post('/api/refresh1', async function (req, res) {
